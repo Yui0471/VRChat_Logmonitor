b7f88a4327d75d93731ab9c6297be9a2fa48a719
Yui-Kazeniwa <wind.garden0471@gmail.com>
2022-08-21 12:14:26 +0900

    printの整理, エラー文の出力をするように

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ import glob
 from pythonosc.udp_client import SimpleUDPClient
 
 moni = """
-############################################
-#                                          #
-#   VRChat Log Monitor                     #
-#                  Version Alpha 1.1.1     #
-#                                          #
-#   Author : Yui-Kazeniwa                  #
-#                                          #
-############################################
+######################################
+#                                    #
+#   VRChat Log Monitor               #
+#                  Version 1.2.2     #
+#                                    #
+#   Author : Yui-Kazeniwa            #
+#                                    #
+######################################
 
 [info] 現在のワールド、インスタンス人数をログから取得します
-[!Warning!] VRChatを起動してから本スクリプトを実行してください
+[Warning!] VRChatを起動してから本スクリプトを実行してください
 """
 
 ip = "127.0.0.1"
@@ -33,8 +33,14 @@ def logfile_detection(directory_path):
             logfiles.append(s)
         else:
             pass
+
+    if len(logfiles) == 0:
+        print("[Error!] ログファイルが見つかりませんでした。VRChatを起動してから実行してください")
+        sys.exit()
     
     latest = max(logfiles, key=os.path.getctime) # 最新のログファイルのパスを取得
+
+    print("[info]", os.path.basename(latest), "ファイルを開きました")
 
     return latest
 
@@ -46,7 +52,7 @@ def directory_move():
     
     try:
         os.chdir(directory_path)
-        # print("[info] VRChatディレクトリに移動しました")
+        print("[info] VRChatディレクトリに移動しました")
         return directory_path
 
     except FileNotFoundError:
@@ -57,7 +63,7 @@ def directory_move():
 # txtを読み込み専用で開きデータを行ごとにリスト化する
 def txt_open(path):
     f = open(path, "r", encoding="utf-8")
-    data = f.read().readlines()
+    data = f.read().split("\n")
 
     txtdata = []
     for s in data:
@@ -80,6 +86,13 @@ def current_world(data):
             worldcount = count # ワールドjoinのログがある行番号を記録
         count += 1 # 行番号をカウント
 
+    if worldcount == 0: # 起動したてでワールド情報がログに出力されていない場合
+        for i in range(10, -1, -1):
+            print("\r[Error!] ワールド情報が取得できませんでした。{}秒後に再取得します".format(i), end="")
+            time.sleep(1)
+
+        return None
+
     worldname_num = world.find("Entering Room:") + 15
     worldname = world[worldname_num :]
     
@@ -95,7 +108,7 @@ def player_count(data, world_count): # list, int, list
     player = []
     del data[: world_count]
 
-    for s in data:
+    for s in data: # ユーザ名だけ使うのでそれ以外は切り捨てる
         if "OnPlayerJoined " in s:
             playername_num = s.find("OnPlayerJoined ") + 15
             playername = s[playername_num :]
@@ -112,6 +125,13 @@ def player_count(data, world_count): # list, int, list
     # プレイヤーがインスタンスにjoinする
     # 2022.08.20 16:28:02 Log        -  [Behaviour] OnPlayerLeft [PlayerName]
     # プレイヤーがインスタンスからLeftする
+
+
+#def time_comparison(worldname):
+    #if world_time_com != worldname:
+
+
+
     
 
 
@@ -121,37 +141,40 @@ if __name__ == "__main__":
     input()
     print("[info] ログの監視を開始します")
     print("[info] IP :", ip, "PORT :", port)
-    print("[info] OSCの送信を開始します\n")
+    print("[info] OSC送信を開始します")
+
+    #print("[Warning!] テスト版のためOSC機能は無効化されています")
 
     client = SimpleUDPClient(ip, port)
 
     filepath = logfile_detection(directory_move())
     player_list = []
+    world_time_com = ""
 
-    try:
-        while True:
+    while True:
+        logdata = txt_open(filepath)
+        world = current_world(logdata)
+
+        while world is None: # 取得できないとNoneが返るので再取得
             logdata = txt_open(filepath)
             world = current_world(logdata)
 
-            player_list = player_count(logdata, world[0])
-            player = len(player_list)
+        player_list = player_count(logdata, world[0])
+        player = len(player_list)
 
-            print("\r", "[info] 現在のワールド", world[1], ": 現在のインスタンス人数", player, "人", end="")
+        print("\r", "[info] 現在のワールド :", world[1], ": 現在のインスタンス人数", player, "人", end="")
 
-            player_str = str(player)
+        player_str = str(player)
 
-            # 恐らく3桁になることはないはず……。
-            if len(player_str) == 1:
-                first = player_str[-1] # 一の位
-                client.send_message("/avatar/parameter/*", int(first))
-                client.send_message("/avatar/parameter/**", 0)
-            else:
-                first = player_str[-1] # 一の位
-                second = player_str[-2] # 十の位
-                client.send_message("/avatar/parameter/*", int(first))
-                client.send_message("/avatar/parameter/**", int(second))
+        # 恐らく3桁になることはないはず……。
+        if len(player_str) == 1:
+            first = player_str[-1] # 一の位
+            client.send_message("/avatar/parameters/DD_op", int(first))
+            client.send_message("/avatar/parameters/DD_tp", 0)
+        else:
+            first = player_str[-1] # 一の位
+            second = player_str[-2] # 十の位
+            client.send_message("/avatar/parameters/DD_op", int(first))
+            client.send_message("/avatar/parameters/DD_tp", int(second))
 
-            time.sleep(1)
-
-    except Exception as e:
-        print(e)
+        time.sleep(1)
