45244681a027e9a228a05b3f848db595592e0aa9
Rounz <83518629+xrun-rounz1@users.noreply.github.com>
2022-09-26 04:30:33 +0900

    Editor拡張の追加

---
+++ Editor/AnimatorUtility.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Animations;
+
+
+class AnimatorUtility {
+    public static AnimatorControllerLayer CloneLayer(AnimatorControllerLayer orig, AnimatorController destController) {
+        //Get Asset Path
+        string assetPath = AssetDatabase.GetAssetPath(destController);
+        AnimatorControllerLayer dest = new AnimatorControllerLayer();
+        dest.name = orig.name;
+        dest.defaultWeight = orig.defaultWeight;
+        dest.avatarMask = orig.avatarMask;
+        dest.iKPass = orig.iKPass;
+        dest.syncedLayerAffectsTiming = orig.syncedLayerAffectsTiming;
+        dest.syncedLayerIndex = orig.syncedLayerIndex;
+        dest.blendingMode = orig.blendingMode;
+
+        if(orig.stateMachine != null) {
+            dest.stateMachine = CloneStateMachine(orig.stateMachine, assetPath);
+            //StateMachineをControllerに保存
+            AssetDatabase.AddObjectToAsset(dest.stateMachine, assetPath);
+        }
+        if(destController != null) {
+            destController.AddLayer(dest);
+        }
+        return dest;
+    }
+
+    public static AnimatorStateMachine CloneStateMachine(AnimatorStateMachine orig, string assetPath) {
+        AnimatorStateMachine dest = new AnimatorStateMachine();
+        List<ChildAnimatorState> destStatesList = new List<ChildAnimatorState>();
+        //StateをClone
+        foreach(var child in orig.states) {
+            AnimatorState state = CloneState(child.state, assetPath);
+            destStatesList.Add(new ChildAnimatorState {
+                state = state,
+                position = child.position
+            });
+            if(assetPath != "") {
+                //StateをControllerに保存
+                AssetDatabase.AddObjectToAsset(state, assetPath);
+            }
+        }
+        dest.states = destStatesList.ToArray();
+
+        dest.anyStatePosition = orig.anyStatePosition;
+        
+
+        List<StateMachineBehaviour> destBehaviours = new List<StateMachineBehaviour>();
+        foreach (var behaviour in orig.behaviours) {
+            StateMachineBehaviour destBehaviour = UnityEngine.Object.Instantiate(behaviour);
+            destBehaviours.Add(destBehaviour);  
+            destBehaviour.name = "";
+            //Instantiateでコピーされないし、HideInHierarchyにしないと、nameが同じになって親がすり替えられる？？？
+            destBehaviour.hideFlags = HideFlags.HideInHierarchy;
+            AssetDatabase.AddObjectToAsset(destBehaviour, assetPath);
+        }
+        dest.behaviours = destBehaviours.ToArray();
+        dest.entryPosition = orig.entryPosition;
+        dest.exitPosition = orig.exitPosition;
+        dest.name = orig.name;
+        dest.hideFlags = orig.hideFlags;
+
+        //Transitionを作成
+        MakeTransition(orig, dest, assetPath);
+        return dest;
+    }
+
+    public static AnimatorState CloneState(AnimatorState orig, string assetPath) {
+        AnimatorState dest = new AnimatorState();
+        //state本体をClone
+        //behaviours
+        List<StateMachineBehaviour> destBehaviours = new List<StateMachineBehaviour>();
+        foreach(var behaviour in orig.behaviours) {
+            StateMachineBehaviour destBehaviour = UnityEngine.Object.Instantiate(behaviour);
+            destBehaviours.Add(destBehaviour);
+            destBehaviour.name = "";
+            //Instantiateでコピーされないし、HideInHierarchyにしないと、nameが同じになって親がすり替えられる？？？
+            destBehaviour.hideFlags = HideFlags.HideInHierarchy;
+            AssetDatabase.AddObjectToAsset(destBehaviour, assetPath);
+        }
+        dest.behaviours = destBehaviours.ToArray();
+
+        dest.cycleOffset = orig.cycleOffset;
+        dest.cycleOffsetParameter = orig.cycleOffsetParameter;
+        dest.cycleOffsetParameterActive = orig.cycleOffsetParameterActive;
+        dest.iKOnFeet = orig.iKOnFeet;
+        dest.mirror = orig.mirror;
+        dest.mirrorParameter = orig.mirrorParameter;
+        dest.mirrorParameterActive = orig.mirrorParameterActive;
+        dest.motion = orig.motion;
+        dest.speed = orig.speed;
+        dest.speedParameter = orig.speedParameter;
+        dest.speedParameterActive = orig.speedParameterActive;
+        dest.writeDefaultValues = orig.writeDefaultValues;
+        dest.name = orig.name;
+        dest.hideFlags = orig.hideFlags;
+
+        //Transition
+        foreach (var transition in orig.transitions) {
+            AnimatorStateTransition destTransition;
+            if(transition.destinationState == null) {
+                //exit
+                destTransition = dest.AddExitTransition();
+            } else {
+                //一旦オリジナルのインスタンスを入れておく
+                destTransition = dest.AddTransition(transition.destinationState);
+            }
+            //Parameter
+            destTransition.canTransitionToSelf = transition.canTransitionToSelf;
+            destTransition.duration = transition.duration;
+            destTransition.exitTime = transition.exitTime;
+            destTransition.hasExitTime = transition.hasExitTime;
+            destTransition.hasFixedDuration = transition.hasFixedDuration;
+            destTransition.interruptionSource = transition.interruptionSource;
+            destTransition.offset = transition.offset;
+            destTransition.orderedInterruption = transition.orderedInterruption;
+            //AnimatorTransitionBase
+            destTransition.isExit = transition.isExit;
+            destTransition.mute = transition.mute;
+            destTransition.solo = transition.solo;
+            destTransition.hideFlags = transition.hideFlags;
+            destTransition.name = transition.name;
+            destTransition.conditions = (AnimatorCondition[])transition.conditions.Clone();
+
+            if(assetPath != "") {
+                //Transitionを保存
+                AssetDatabase.AddObjectToAsset(destTransition, assetPath);
+            }
+        }
+        return dest;
+    }
+
+
+    private static void MakeTransition(AnimatorStateMachine orig, AnimatorStateMachine dest, string assetPath) {
+        //Entry
+        ChildAnimatorState[] origChild = orig.states;
+        ChildAnimatorState[] destChild = dest.states;
+        foreach (var transition in orig.entryTransitions) {
+            //TransitionのDestinationを取得
+            AnimatorState state = origChild
+                                    .Select((child, index) => new { child, index})
+                                    .Where(obj => obj.child.state == transition.destinationState)
+                                    .Select((obj) => destChild[obj.index].state).First();
+            //TransitionをClone
+            AnimatorTransition destTransition = dest.AddEntryTransition(state);
+            destTransition.isExit = transition.isExit;
+            destTransition.mute = transition.mute;
+            destTransition.solo = transition.solo;
+            destTransition.hideFlags = transition.hideFlags;
+            destTransition.name = transition.name;
+            //conditions
+            destTransition.conditions = (AnimatorCondition[])transition.conditions.Clone();
+
+            if (assetPath != "") {
+                //Transitionを保存
+                AssetDatabase.AddObjectToAsset(destTransition, assetPath);
+            }
+
+
+        }
+
+        //オリジナルのものを置き換え
+        foreach(var state in destChild) {
+            foreach(var transition in state.state.transitions) {
+                //行き先がないものは無視
+                if(transition.destinationState == null) {
+                    continue;
+                }
+                AnimatorState clonedState = origChild
+                                    .Select((child, index) => new { child, index })
+                                    .Where(obj => obj.child.state == transition.destinationState)
+                                    .Select((obj) => destChild[obj.index].state).First();
+                transition.destinationState = clonedState;
+            }
+        }
+
+        //defaultState
+        dest.defaultState = origChild
+                                    .Select((child, index) => new { child, index })
+                                    .Where(obj => obj.child.state == orig.defaultState)
+                                    .Select((obj) => destChild[obj.index].state).First();
+
+    }
+
+}
---
+++ Editor/LogMonitorWindow.cs
@@ -0,0 +1,194 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+using UnityEditor.Animations;
+using VRC.SDK3.Avatars.Components;
+using VRC.SDK3.Avatars.ScriptableObjects;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+public class LogMonitorUIElement : EditorWindow {
+    private const string LAYOUT_UXML_GUID = "37e2f1ca14323f744af4615403374007";
+    private const string STYLESHEET_USS_GUID = "28c045968fb2a274f9635f37490406fd";
+
+    private string strErrorHelpBox = "";
+    // メニューに追加
+    [MenuItem("Yuis Miniature Garden/Log Monitor")]
+    public static void ShowWindow() {
+        // ウィンドウの作成
+        var wnd = GetWindow<LogMonitorUIElement>();
+        // ウィンドウタイトルの設定
+        wnd.titleContent = new GUIContent("Log Monitor");
+    }
+    
+    private void OnEnable() {
+        VisualTreeAsset tree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(AssetDatabase.GUIDToAssetPath(LAYOUT_UXML_GUID));
+
+        VisualElement root = tree.CloneTree();
+
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>(AssetDatabase.GUIDToAssetPath(STYLESHEET_USS_GUID));
+        root.styleSheets.Add(styleSheet);
+
+        Button button = root.Q<Button>("add_button");
+        button.SetEnabled(false);
+
+        IMGUIContainer imguiErrBox = root.Q<IMGUIContainer>("error_helpbox");
+        imguiErrBox.onGUIHandler += () => {
+            EditorGUILayout.HelpBox(strErrorHelpBox, MessageType.Error);
+        };
+        imguiErrBox.visible = false;
+
+        ObjectField field = root.Q<ObjectField>("vrcdescriptor_objfield");
+        field.objectType = typeof(VRCAvatarDescriptor);
+        field.RegisterValueChangedCallback(e => {
+            Reset();
+        });
+
+
+        button.clicked += () => {
+
+            VRCAvatarDescriptor descriptor = field.value as VRCAvatarDescriptor;
+
+            LogMonitorInstall.Install(descriptor);
+
+        };
+
+
+        rootVisualElement.Add(root);
+
+    }
+
+    void SetCostGage(int max, int used, int require) {
+        VisualElement root = this.rootVisualElement;
+        Box elmUsed = root.Q<Box>("avatar_paramcost_used");
+        Box elmRequire = root.Q<Box>("avatar_paramcost_require");
+        Label elmUseLabel = root.Q<Label>("avatar_paramcost_labeluse");
+        Label elmFreeLabel = root.Q<Label>("avatar_paramcost_labelfree");
+        
+        elmUsed.style.width = new StyleLength(Length.Percent(used / (max * 1.0f) * 100.0f));
+        elmRequire.style.width = new StyleLength(Length.Percent(require / (max * 1.0f) * 100));
+
+        if (max == 0) {
+            elmUseLabel.text = " ";
+            elmFreeLabel.text = " ";
+        } else {
+            elmUseLabel.text = $"{used} + {require} ({used+require}) / {max}";
+            elmFreeLabel.text = $"空き: {max - used} - {require} ({max - used - require})";
+        }
+    }
+
+    int CalcExParamCost(VRCExpressionParameters exparams) {
+        if(exparams == null) {
+            return 0;
+        }
+
+        var validParamsType = from param in exparams.parameters
+                          where param.name != ""
+                          select param.valueType;
+
+        int total = 0;
+        foreach(var paramType in validParamsType) {
+            total += VRCExpressionParameters.TypeCost(paramType);
+        }
+        return total;
+    }
+
+    private void OnFocus() {
+        //Debug.Log("OnFocus()");
+
+        IMGUIContainer imguiErrBox = this.rootVisualElement.Q<IMGUIContainer>("error_helpbox");
+        ObjectField field = this.rootVisualElement.Q<ObjectField>("vrcdescriptor_objfield");
+
+        //ファイルチェック
+        if (!LogMonitorInstall.CheckBroken()) {
+            Debug.Log("test");
+            strErrorHelpBox = "パッケージが破損しています。UnityPackageを再インポートしてください。";
+            
+            imguiErrBox.visible = true;
+            imguiErrBox.MarkDirtyRepaint();
+            field.SetEnabled(false);
+            return;
+        } else {
+            strErrorHelpBox = "";
+            imguiErrBox.visible = false;
+            imguiErrBox.MarkDirtyRepaint();
+            field.SetEnabled(true);
+        }
+        Reset();
+        
+    }
+
+    public void Reset() {
+        VisualElement root = this.rootVisualElement;
+        Button button = root.Q<Button>("add_button");
+        if(button == null) {
+            return;
+        }
+        button.SetEnabled(false);
+
+        IMGUIContainer imguiErrBox = root.Q<IMGUIContainer>("error_helpbox");
+
+        ObjectField field = root.Q<ObjectField>("vrcdescriptor_objfield");
+        field.objectType = typeof(VRCAvatarDescriptor);
+
+        if (field.value != null && field.value is VRCAvatarDescriptor) {
+            VRCAvatarDescriptor descriptor = (VRCAvatarDescriptor)field.value;
+
+            int used = CalcExParamCost(descriptor.expressionParameters);
+
+            SetCostGage(VRCExpressionParameters.MAX_PARAMETER_COST, used, LogMonitorInstall.GetRequireParamCost());
+
+
+            CheckErrorCode err = LogMonitorInstall.DescriptorCheck(descriptor);
+
+            switch (err) {
+                case CheckErrorCode.MISSING_ANIMATOR:
+                    strErrorHelpBox =
+                        $"\"{descriptor.gameObject.name}\"にはAnimator Componentが存在しません";
+                    break;
+
+                case CheckErrorCode.INVALID_AVATAR:
+                    strErrorHelpBox =
+                        $"\"{descriptor.gameObject.name}\"のAnimatorにAvatarがセットされていないか、\n" +
+                        "無効なAvatarです";
+                    break;
+
+                case CheckErrorCode.NOT_HUMANOID:
+                    strErrorHelpBox =
+                        $"\"{descriptor.gameObject.name}\"のAvatarはHumanoidではありません。\n" +
+                        $"Humanoidである必要があります。";
+                    break;
+
+                case CheckErrorCode.MISSING_NECKBONE:
+                    strErrorHelpBox =
+                        $"\"{descriptor.gameObject.name}\"にはNeckボーンが存在しないか、Mappingにセットされていません。\n" +
+                        $"Neck、Headボーンが必要です。";
+                    break;
+
+                case CheckErrorCode.MISSING_HEADBONE:
+                    strErrorHelpBox =
+                        $"\"{descriptor.gameObject.name}\"にはHeadボーンが存在しないか、Mappingにセットされていません。\n" +
+                        $"Neck、Headボーンが必要です。";
+                    break;
+
+            }
+
+            if (err != CheckErrorCode.SUCCESS) {
+                imguiErrBox.visible = true;
+                button.SetEnabled(false);
+            } else {
+                imguiErrBox.visible = false;
+                strErrorHelpBox = "";
+                button.SetEnabled(true);
+            }
+        } else {
+            imguiErrBox.visible = false;
+            SetCostGage(0, 0, 0);
+            button.SetEnabled(false);
+        }
+    }
+
+}
